c63c2040fab96ab774d804083a085be670c5aceb
michellespengler <55745000+michellespengler@users.noreply.github.com>
2021-03-25 17:36:24 +0100

    first commit tap it game

---
+++ TapIt/script.js
@@ -0,0 +1,4 @@
+//<button onclick="window.location.href='inicio.html'">Continue</button>
+
+//document.querySelector(".button_next").addEventListener("click",function() {
+ //   window.location.href = 'inicio.html';   })
