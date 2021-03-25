a9a7c23b55d10bce712c5bfe3bc4490a248fe2d3
michellespengler <55745000+michellespengler@users.noreply.github.com>
2021-03-25 18:00:50 +0100

    Delete TapIt directory
    etwas

--- TapIt/script.js
+++
@@ -1,4 +0,0 @@
-//<button onclick="window.location.href='inicio.html'">Continue</button>
-
-//document.querySelector(".button_next").addEventListener("click",function() {
- //   window.location.href = 'inicio.html';   })
